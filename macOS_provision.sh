73fb09fd188f8614e266da6c9818f763c621188c
Michael E Fisher <fisherm@dnc.org>
2017-03-25 11:38:06 -0400

    be more specific

--- macOS_provision.sh
+++ macOS_provision.sh
@@ -9,7 +9,7 @@ fi
 
 # get pip and install ansible
 
-curl https://bootstrap.pypa.io/get-pip.py > /tmp/get-pip.py
-sudo python /tmp/get-pip.py
+/usr/bin/curl https://bootstrap.pypa.io/get-pip.py > /tmp/get-pip.py
+sudo /usr/bin/python /tmp/get-pip.py
 # install ansible and only run playbook if that succeeds
 sudo pip install ansible && ansible-playbook provision_vm.yml
