3a201baf764bcd296870775846dcaf02eeaff272
Michael E Fisher <fisher@corybooker.com>
2020-01-17 16:16:17 -0500

    add pipenv - was missing

--- provision.sh
+++ provision.sh
@@ -3,7 +3,7 @@
 ANSIBLE_VAULT_PASSWORD_FILE='./ansible_password'
 
 if [[ -f $ANSIBLE_VAULT_PASSWORD_FILE ]]; then
-    ansible-playbook -i hosts provision.yml --ask-become-pass --tags "${TAGS}" "$1" "$2"
+    pipenv run ansible-playbook -i hosts provision.yml --ask-become-pass --tags "${TAGS}" "$1" "$2"
 else
 	echo "There should exist a password file: ansible_password"
 fi
