0fc42e56c9e68ef15b1c5f32a444351c874ead37
Michael E Fisher <fisher@streak.com>
2020-11-04 22:11:49 -0500

    provisioning updates

--- provision.sh
+++ provision.sh
@@ -3,7 +3,7 @@
 ANSIBLE_VAULT_PASSWORD_FILE='/Users/michael/boxen-create/ansible_password'
 
 if [[ -f $ANSIBLE_VAULT_PASSWORD_FILE ]]; then
-    pipenv run ansible-playbook -i hosts main.yml --tags "${TAGS}" "$1" "$2" --ask-become-pass
+    pipenv run ansible-playbook -i hosts main.yml --tags "${TAGS}" "$1" "$2" --ask-become-pass "$3"
 else
 	echo "There should exist a password file: ansible_password"
 fi
