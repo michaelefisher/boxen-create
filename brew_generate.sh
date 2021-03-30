203808145c818c85fddf3a2db99e15e0092bea6b
Michael E Fisher <fisher@streak.com>
2021-03-30 16:34:38 -0400

    add requested apps to brew cask

--- brew_generate.sh
+++ brew_generate.sh
@@ -1,24 +1,26 @@
 #!/usr/bin/env bash
 
 which -s brew
-if [[ $? == 0 && $(brew list --formula) ]]; then
-  formulas=$(brew list --formula -1)
-elif [[ $? == 0 ]]; then
-  formulas=$(cat brew_formulas.txt)
+if [[ $? == 0 ]]; then
+  formulas_installed=$(brew list -1)
+  formulas_requested=$(cat brew.txt | sort)
 else
   echo 'Homebrew not installed. Please install Homebrew at https://brew.sh.\n'
 fi
 
-echo "${formulas}" | sed -e :a -e '$!N; s/\n/\", \"/; ta' | sed 's/^/\[\"/' | sed 's/$/\"\]/' > roles/common/templates/brew_formulas.txt
+if [[ `diff <(echo "$formulas_installed") <(echo "$formulas_requested")` ]]; then
+  echo "${formulas_requested}" | sed -e :a -e '$!N; s/\n/\", \"/; ta' | sed 's/^/\[\"/' | sed 's/$/\"\]/' > roles/common/templates/brew_formulas.txt
+fi
 
 which -s brew
-if [[ $? == 0 && $(brew list --cask) ]]; then
-  formulas=$(brew list --cask -1)
-elif [[ $? == 0 ]]; then
-  formulas=$(cat brew_cask.txt)
+if [[ $? == 0 ]]; then
+  formulas_installed=$(brew list --cask -1)
+  formulas_requested=$(cat brew_cask.txt | sort)
 else
   echo 'Homebrew not installed. Please install Homebrew at https://brew.sh.\n'
 fi
 
-echo "${formulas}" | sed -e :a -e '$!N; s/\n/\", \"/; ta' | sed 's/^/\[\"/' | sed 's/$/\"\]/' > roles/common/templates/brew_cask.txt
+if [[ `diff <(echo "$formulas_installed") <(echo "$formulas_requested")` ]]; then
+  echo "${formulas_requested}" | sed -e :a -e '$!N; s/\n/\", \"/; ta' | sed 's/^/\[\"/' | sed 's/$/\"\]/' > roles/common/templates/brew_cask.txt
+fi
 
