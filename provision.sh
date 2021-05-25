94e4c76f7e05d904d65d8bfa1e9c211be83a17ae
Michael E Fisher <fisher@streak.com>
2021-05-25 10:36:56 -0400

    refactor brew generate

--- provision.sh
+++ provision.sh
@@ -7,7 +7,15 @@ set +x
 ANSIBLE_VAULT_PASSWORD_FILE='/Users/michael/boxen-create/ansible_password'
 
 help="Ansible provisioning wrapper\n\nUsage:\n\n./provision.sh --tags \$1 --limit \$2\n\nYou can add multiple comma-separated tags and limits.\n\nExample:\n\n./provision.sh --tags files --limit kermit-the-frog"
-version=1.0.0
+version=1.1.0
+
+function brew_generate() {
+ if [[ -z $tags || $tags == "homebrew"* ]]; then
+    echo "Generating brew list for Ansible..."
+    bash -c ". brew_generate.sh"
+    echo "Done!"
+  fi
+}
 
 [[ -z "$1" ]] && echo "Must include either tags or limit flags." && exit;
 POSITIONAL=()
@@ -25,6 +33,10 @@ while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do case $1 in
   -l | --limit )
     shift; limit=$1
     ;;
+  -b | --brew )
+    brew_generate
+    exit
+    ;;
   -h | --help )
     shift; echo -e $help
     exit
@@ -35,13 +47,9 @@ while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do case $1 in
 esac; shift; done
 if [[ "$1" == '--' ]]; then shift; fi
 set -- "${POSITIONAL[@]}"
-
 if [[ -f $ANSIBLE_VAULT_PASSWORD_FILE ]]; then
-  if [[ -z $tags || $tags == "homebrew"* ]]; then
-    echo "Generating brew list for Ansible..."
-    bash -c ". brew_generate.sh"
-  fi
-  echo "Running: $roles for tags=$tags, limit=$limit $POSITIONAL"
+   brew_generate;
+   echo "Running: $roles for tags=$tags, limit=$limit $POSITIONAL"
   if [[ -n $tags ]]; then
     pipenv run ansible-playbook ${roles}.yml -i hosts.yml --tags=$tags --limit=$limit $POSITIONAL
   else
@@ -50,3 +58,5 @@ if [[ -f $ANSIBLE_VAULT_PASSWORD_FILE ]]; then
 else
   echo "There should exist a password file: ansible_password"
 fi
+
+
